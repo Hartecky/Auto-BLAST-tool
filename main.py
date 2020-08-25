963f464be80d204f46a0f15964e0db90d6c066ac
Bartłomiej Hofman <59232088+Hartecky@users.noreply.github.com>
2020-08-25 13:46:17 +0200

    Added simple GUI with AppJar

--- main.py
+++ main.py
@@ -11,18 +11,70 @@ from redirect_page import redirect_page
 from check_file import check_file
 from download_seq import download_sequences
 
+from appJar import gui
+
 import time
 import os
 
-input_query = str(input("Please submit your sequence: \n"))
-input_organism = str(input("\nPlease submit your organism name: \n"))
+#input_query = str(input("Please submit your sequence: \n"))
+#input_organism = str(input("\nPlease submit your organism name: \n"))
 
-def main():
+
+def search_blast():
+
+    #if name == "Submit Auto-Blast":
+    seq = app.getEntry("Sequence")
+    org = app.getEntry("Organism")
+
     web_browser = open_blast()
-    submit_values(web_browser, input_query, input_organism)
+    submit_values(web_browser, seq, org)
     redirect_page(web_browser)
     download_sequences(web_browser)
+    web_browser.close()
+
+
+
+def main():
+
+    def press(name):
+        if name == "Cancel":
+            app.stop()
+        elif name == "Clear":
+            app.clearEntry("Sequence")
+            app.clearEntry("Organism")
+            app.setFocus("Sequence")
+
+    def search_blast():
+
+        #if name == "Submit Auto-Blast":
+        seq = app.getEntry("Sequence")
+        org = app.getEntry("Organism")
+
+        web_browser = open_blast()
+        submit_values(web_browser, seq, org)
+        redirect_page(web_browser)
+        download_sequences(web_browser)
+        web_browser.close()
+
+    app = gui("Auto-Blast Tool", "640x480")
+    app.setBg("green")
+    app.setFont(20)
+    app.setResizable(False)
+    app.addLabel("title", "Auto-Blast Tool")
+
+    app.addLabelEntry("Sequence")
+    app.addLabelEntry("Organism")
+
+    app.addButton("Submit Auto-Blast", search_blast)
+    app.addButtons(["Clear", "Cancel"], press)
+    app.go()
 
 
 if __name__ == '__main__':
     main()
+
+'''
+Example sequence for arabidopsis thaliana PetC Gene
+
+GCCTTATCTGCTTCTCTCTCTCCAGTGGTCTCAAATCTCTCTGTGTGATCCCACTTGTCATCCTCCTCCACAACATAAAGCTTGAGACCATAACCCTTTAGCGTTTTCGGCTAAAGCTTTCGCTGACTACTACAACAATGGCGTCCTCATCCCTTTCCCCTGCTACTCAGGTTCACTCTTGATCGTTCGAATCGAAACAATCGGGTCTCTTCGTATAGGAATTTGGGTTTTTGAAAGTTTGGTTTTTTTTTTTGGTGGCAGCTTGGTTCTAGCAGAAGTGCTTTGATGGCGATGTCAAGTGGGTTGTTTGTGAAGCCAACGAAGATGAATCATCAAATGGTTAGAAAAGAGAAGATTGGATTGAGAATTTCTTGTCAAGCGTCGAGTATTCCAGCAGACAGAGTTCCAGATATGGAAAAGAGGAAGACTTTGAATCTTCTTCTTCTTGGGGCTCTTTCTCTACCTACTGGCTACATGCTTGTCCCTTACGCTACCTTCTTTGTTCCTCCTGGAACCGGAGGTGGAGGTGGTGGTACTCCAGCCAAGGATGCCCTTGGAAACGATGTAGTTGCAGCGGAATGGCTTAAGACTCATGGTCCCGGTGACCGAACCTTGACCCAAGGATTAAAGGGAGATCCGACTTACCTAGTTGTAGAGAACGACAAGACTCTAGCGACATACGGTATCAACGCAGTGTGCACTCATCTTGGATGTGTTGTGCCATGGAACAAAGCTGAGAACAAGTTTCTATGTCCTTGCCATGGATCCCAATACAACGCCCAAGGAAGAGTCGTTAGAGGTCCAGCCCCATTGTCGCTAGCGTTGGCTCACGCGGATATAGATGAAGCTGGGAAGGTTCTTTTTGTTCCATGGGTGGAAACTGACTTCAGGACTGGTGATGCTCCATGGTGGTCTTAAGACTCTTCAACAAGAAAAAGAGAAAGATTTGGTCTTTTTGTGTAAGACTTGTTTGAATGTTCTTATAATGTATAAGCTACATTTCATCGCAATTACTCTGTGAATTCTTGCCTCTTATATGAAATATTATGTTCATTCACTTCCCA
+'''
