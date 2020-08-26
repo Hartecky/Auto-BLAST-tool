7cc97b5e329e354bb37b9b036c03c5dbc7a58d4e
Bartłomiej Hofman <59232088+Hartecky@users.noreply.github.com>
2020-08-26 15:22:01 +0200

    Added error handling exception (in progress)

--- submit_query.py
+++ submit_query.py
@@ -5,7 +5,7 @@ from selenium.webdriver.support.ui import Select
 import time
 import os
 
-def submit_values(driver, query, organism):
+def fill_values(driver, query, organism):
     """ Sending desired queries into textboxes, matching algorithm for 10000 sequences search and submitting blast algorithn
 
     :param driver: webdriver defined by function open_blast() in a open_browser.py file
@@ -37,8 +37,26 @@ def submit_values(driver, query, organism):
 
     # Find and choose searching target
     select = Select(driver.find_element_by_id("NUM_SEQ"))
-    select.select_by_value("10000")
+    select.select_by_value("5000")
 
     # Run
     blast = driver.find_element_by_class_name("blastbutton")
     blast.click()
+
+    return(driver)
+
+
+def refresh_page(driver):
+    """ There will be here error with which this function will handle with"""
+    driver.refresh()
+
+
+def submit_query(driver, query, organism):
+    """Function made for handling BLAST main web-page in case of some CGI Text Error """
+
+    try:
+        fill_values(driver, query, organism)
+
+    except:
+        refresh_page()
+        fill_values(driver, query, organism)
