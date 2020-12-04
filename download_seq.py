bab77c054866c89b3b7f852e36ddc99cb6d2867e
Bartłomiej Hofman <59232088+Hartecky@users.noreply.github.com>
2020-12-04 12:38:35 +0100

    added docstrings

--- download_seq.py
+++ download_seq.py
@@ -5,14 +5,24 @@ from selenium.webdriver import ActionChains
 import os
 import time
 
+# Destination paths
 file_path_1 = os.path.expanduser("~")+"\\Downloads\\downloaded_sequences.txt"
 file_path_2 = os.path.expanduser("~")+"\\Downloads\\seqdump.txt"
-
 download_folder = os.path.expanduser("~")+"\\Downloads"
 
 def check_file(file_to_check, path_1, path_2):
-    """Checks if a file 'seqdump.txt' is in a directory, otherwise it waits untill it will appear
-    :param file_to_check: path to a file which we want to download and we are waiting for
+    """
+    Checks if a file 'seqdump.txt' is in a directory,
+    otherwise it waits untill it will appear
+
+    Parameters:
+    file_to_check: file name with a path, which we want to
+    download. The while loop is waiting for confirmed presence
+    of a provided file.
+
+    path_1: Downloads directiory path with a file name to be renamed.
+
+    path_2: File path with a name of a downloaded data.
     """
 
     while not os.path.exists(file_to_check):
@@ -23,11 +33,19 @@ def check_file(file_to_check, path_1, path_2):
     else:
         raise ValueError("%s is not a file" % file_to_check)
 
+    return
+
 
 def download_sequences(driver):
-    """Download all avalaible sequences returned in a query search provided by function submit_values() in a submit_query.py file. Runs only if redirect_page() will find avalaible sequences in a blast search
+    """
+    Downloads all avalaible sequences returned in a query
+    search provided by function submit_values() in a submit_query.py file.
+    Runs only if redirect_page() will find avalaible sequences in a blast search.
 
-    :param driver: webdriver defined by function open_blast() in a open_browser.py file
+    Parameters:
+    driver: selenium.webdriver object used in another functions.
+    Defined by function open_blast() in a open_browser.py file
+
     """
 
     # On a redirected page find and choose download option
@@ -40,5 +58,6 @@ def download_sequences(driver):
     # Function call to check downloading file existing
     check_file(file_path_2, file_path_1, download_folder)
 
-    return(driver)
+    return
+
 
