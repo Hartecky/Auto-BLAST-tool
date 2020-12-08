6a27d676ccb02ab1454a0cf31870b5ab8c16134e
Bartłomiej Hofman <59232088+Hartecky@users.noreply.github.com>
2020-12-08 15:36:25 +0100

    Update arguments.py

--- arguments.py
+++ arguments.py
@@ -1,24 +1,27 @@
 import argparse
 
-# ---------- START ---------- #
-parser = argparse.ArgumentParser(description='Remote sequence downloader from NCBI Nucleotide Blast database')
 
-# ---------- POSITIONALS ---------- #
-parser.add_argument('-q', '--query', type=str, metavar='', required=True, help='Nucleotide sequence query')
+def parse_arguments():
+    # ---------- START ---------- #
+    parser = argparse.ArgumentParser(description='Remote sequence downloader from NCBI Nucleotide Blast database')
 
-parser.add_argument('-o', '--organism', type=str, metavar='', required=True, help='Name of the organism input')
+    # ---------- POSITIONALS ---------- #
+    parser.add_argument('-q', '--query', type=str, metavar='', required=True, help='Nucleotide sequence query')
 
-# ---------- OPTIONALS ---------- #
-group = parser.add_mutually_exclusive_group()
+    parser.add_argument('-o', '--organism', type=str, metavar='', required=True, help='Name of the organism input')
 
-group.add_argument('-e', '--exclude', type=str, metavar='', required=False, help='Exclude provided organism (True / False)')
+    # ---------- OPTIONALS ---------- #
+    group = parser.add_mutually_exclusive_group()
 
-group.add_argument('-n', '--number_seq', type=str, metavar='', required=False, help='Select maximum numbers of aligned sequences to download (10 / 50 / 100 / 250 / 500/ 1000/ 5000 avalaible)')
+    group.add_argument('-e', '--exclude', type=str, metavar='', required=False, help='Exclude provided organism (True / False)')
 
-group.add_argument('-a', '--aligned', type=str, metavar='', required=False, help='Download aligned sequences (True / False)')
+    group.add_argument('-n', '--number_seq', type=str, metavar='', required=False, help='Select maximum numbers of aligned sequences to download (10 / 50 / 100 / 250 / 500/ 1000/ 5000 avalaible)')
 
-# ---------- PARSE ARGS ---------- #
-args = parser.parse_args()
+    group.add_argument('-a', '--aligned', type=str, metavar='', required=False, help='Download aligned sequences (True / False)')
 
-if __name__ == '__main__':
-    main()
+    # ---------- PARSE ARGS ---------- #
+    args = parser.parse_args()
+
+    return args
+
+
