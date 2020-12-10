3094bfaa77d854c7d63fea3a5a96d0dac2dc9afd
Bartłomiej Hofman <59232088+Hartecky@users.noreply.github.com>
2020-12-10 22:44:46 +0100

    Added executable PATH

--- open_browser.py
+++ open_browser.py
@@ -14,8 +14,8 @@ driver: selenium.webdriver object used in another functionse.
 def get_driver():
 
     # Path to webdriver API file
-    PATH = "C:/msedgedriver.exe"
-    driver = webdriver.Edge(PATH)
+    #PATH = "C:/msedgedriver.exe"
+    driver = webdriver.Edge(executable_path=r'C:/msedgedriver.exe')
 
     return(driver)
 
