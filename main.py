69e4ef3fa41fb71912bd7fcb3d716b1988c6b17a
Bartłomiej Hofman <59232088+Hartecky@users.noreply.github.com>
2020-12-11 15:20:01 +0100

    function ordering

--- main.py
+++ main.py
@@ -4,28 +4,36 @@ from download_seq import *
 from redirect_page import *
 from arguments import *
 
+
+
+
 def main():
-    # Generate arguments
-    args = parse_arguments()
 
     # Open browser functions
     driver = get_driver()
+    action = make_action(driver)
     open_website(driver)
 
-    # Submit query functions
-    action = make_action(driver)
+    # Submit query main functions
     paste_sequence(driver, args.query)
     paste_organism(driver, action, args.organism)
+
+    # Check if -e flag is True
+    exclude_organism(driver, args.exclude)
+
+    # Submit additional options
     open_options(driver, action)
     select_options(driver, action)
     click_button(driver)
 
-    print("Redirection start")
     # Redirecting page and downloading seq
-    redirect_page(driver)
+    proceed_submit(driver)
     download_sequences(driver)
-    
-    
+    driver.quit()
+
 if __name__ == '__main__':
+
+    # Generate arguments
+    args = parse_arguments()
     main()
 
