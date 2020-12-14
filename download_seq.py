94ab6884bfc9141741b36696d13aae74bbb7ccad
Bartłomiej Hofman <59232088+Hartecky@users.noreply.github.com>
2020-12-14 15:15:57 +0100

    Added option handling + docstrings

--- download_seq.py
+++ download_seq.py
@@ -5,15 +5,10 @@ from selenium.webdriver import ActionChains
 import os
 import time
 
-# Destination paths
-file_path_1 = os.path.expanduser("~")+"\\Downloads\\downloaded_sequences.txt"
-file_path_2 = os.path.expanduser("~")+"\\Downloads\\seqdump.txt"
-download_folder = os.path.expanduser("~")+"\\Downloads"
-
-def check_file(file_to_check, path_1, path_2):
+def check_file(file_to_check, path_1):
     """
     Checks if a file 'seqdump.txt' is in a directory,
-    otherwise it waits untill it will appear
+    otherwise it waits untill it will appear.
 
     Parameters:
     file_to_check: file name with a path, which we want to
@@ -22,21 +17,18 @@ def check_file(file_to_check, path_1, path_2):
 
     path_1: Downloads directiory path with a file name to be renamed.
 
-    path_2: File path with a name of a downloaded data.
     """
 
     while not os.path.exists(file_to_check):
         time.sleep(1)
 
     if os.path.isfile(file_to_check):
-        os.rename(file_to_check, file_path_1)
+        os.rename(file_to_check, path_1)
     else:
         raise ValueError("%s is not a file" % file_to_check)
 
-    return
 
-
-def download_sequences(driver):
+def download_button(driver):
     """
     Downloads all avalaible sequences returned in a query
     search provided by function submit_values() in a submit_query.py file.
@@ -51,12 +43,43 @@ def download_sequences(driver):
     download_button = driver.find_element_by_xpath("//*[@id='btnDwnld']")
     download_button.click()
 
-    download_fasta = driver.find_element_by_xpath("//*[@id='dwFSTAl']")
-    download_fasta.click()
-
-    # Function call to check downloading file existing
-    check_file(file_path_2, file_path_1, download_folder)
-
     return
 
+def download_sequences(driver, option, output_name):
 
+    # Destination paths
+    file_path_1 = os.path.expanduser("~")+"\\Downloads\\"+str(output_name)
+    file_path_2 = os.path.expanduser("~")+"\\Downloads\\seqdump.txt"
+    download_folder = os.path.expanduser("~")+"\\Downloads"
+
+    if output_name is None:
+        file_path_1 = os.path.expanduser("~")+"\\Downloads\\"+"output.txt"
+    else:
+        pass
+
+
+    if str(option).lower() == 'aligned':
+
+        download_all = driver.find_element_by_xpath("//*[@id='dwFSTAl']")
+        download_all.click()
+
+        # Function call to check downloading file existing
+        check_file(file_path_2, file_path_1)
+
+    elif str(option).lower() == 'complete':
+
+        download_aligned = driver.find_element_by_xpath("//*[@id='dwFST']")
+        download_aligned.click()
+
+        # Function call to check downloading file existing
+        check_file(file_path_2, file_path_1)
+
+    else:
+
+        download_aligned = driver.find_element_by_xpath("//*[@id='dwFSTAl']")
+        download_aligned.click()
+
+        # Function call to check downloading file existing
+        check_file(file_path_2, file_path_1)
+
+    return
